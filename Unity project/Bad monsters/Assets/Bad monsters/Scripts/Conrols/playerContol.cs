2b72d86c99fcc2cea4e9b16bcd50fbe48e3607ef
Gennyi <genkenny@mail.ru>
2014-03-20 23:16:11 +0400

    Первая тестовая версия
    1) примитивное управление

---
+++ Unity project/Bad monsters/Assets/Bad monsters/Scripts/Conrols/playerContol.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerContol : MonoBehaviour {
+
+	[HideInInspector]
+	public bool facingRight = true;
+
+	[HideInInspector]
+	//Можем ли мы управлять персонажем
+	public bool isActive = true;
+
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Направление движения
+		float h = Input.GetAxis("Horizontal");
+
+		if(h * rigidbody2D.velocity.x < maxSpeed) {
+			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		}
+
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed) {
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+	}
+}
