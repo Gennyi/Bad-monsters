a085cfa5b84ff1dfbb68ca9ab2f643e48fa65638
Gennadiy Kupriyanov <g.kupriyanov@globalfxonline.ru>
2014-04-23 12:01:46 +0400

    Release 14
    Начали создавать тестовый уровень

--- Unity project/Bad monsters/Assets/Bad monsters/Scripts/Camera/CameraControl.cs
+++ Unity project/Bad monsters/Assets/Bad monsters/Scripts/Camera/CameraControl.cs
@@ -2,46 +2,28 @@
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
-	public const int size = 10;
-	const float infinity = 999999f;
 	
 	public Vector3[,] points;
 	const float widhtScreen = 12.8f;
 	
-	public float doorWidht = 5f;
+	public float doorWidht = 3.5f;
 	public float speed = 2f;
 	
 	private playerControl playerCtrl;
 	// Наша текущая позиция камеры (среди комнат)
 	public Vector2 currentPos = new Vector2(5,5);
 	// Наша предыдущая позиция камеры
+	[HideInInspector]
 	public Vector2 prePos;
 	//Состояния камеры
+	[HideInInspector]
 	public bool isFree = false;
 
 	// Use this for initialization
 	void Start () {
 		prePos = currentPos;
-		points = new Vector3[size, size];
 		playerCtrl = GameObject.Find("Player").GetComponent<playerControl>();
-		for (int i = 0; i < size; i++){
-			for(int j = 0; j < size; j++) {
-				points[i, j] = new Vector3(infinity, infinity, -10f);
-			}
-		}
-		points[3,5].x = -9.2f;
-		points[3,5].y = 4.3f;
-		points[4,5].x = -3.2f;
-		points[4,5].y = 4.3f;
-		points[5,5].x = 4.8f;
-		points[5,5].y = 4.3f;
-		points[6,5].x = 17f;
-		points[6,5].y = 4.3f;
-		points[6,4].x = 15.2f;
-		points[6,4].y = -3f;
-		points[5,4].x = 5f;
-		points[5,4].y = -3f;
-		
+		points = CameraPositions.loadFirstLvl();
 	}
 	
 	// Update is called once per frame
---
+++ Unity project/Bad monsters/Assets/Bad monsters/Scripts/Camera/CameraPositions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPositions : MonoBehaviour {
+
+	private const int size = 10;
+	const float infinity = 999999f;
+	private static Vector3[,] points;
+
+	// Use this for initialization
+	public static Vector3[,] loadFirstLvl() {
+		points = new Vector3[size, size];
+		for (int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++) {
+				points[i, j] = new Vector3(infinity, infinity, -10f);
+			}
+		}
+		
+		points[3,5].x = -9.2f;
+		points[3,5].y = 4.3f;
+		points[4,5].x = -3.2f;
+		points[4,5].y = 4.3f;
+		points[5,5].x = 4.8f;
+		points[5,5].y = 4.3f;
+		points[6,5].x = 17f;
+		points[6,5].y = 4.3f;
+		points[6,4].x = 15.2f;
+		points[6,4].y = -3f;
+		points[5,4].x = 5f;
+		points[5,4].y = -3f;
+
+		return points;
+	}
+}
